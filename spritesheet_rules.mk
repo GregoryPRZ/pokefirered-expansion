c9c4361619956d7be5723b6f35b2b2d78883b358
GregoryPRZ <gregoryp161@gmail.com>
2025-08-16 21:08:02 +0200

    adding hgss trainer sprites and overworlds (fixes to do)

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -148,7 +148,7 @@ $(OBJEVENTGFXDIR)/people/little_boy.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 2
 
 $(OBJEVENTGFXDIR)/people/little_girl.4bpp: %.4bpp: %.png
-	$(GFX) $< $@ -mwidth 2 -mheight 2
+	$(GFX) $< $@ -mwidth 2 -mheight 4
 
 $(OBJEVENTGFXDIR)/people/lorelei.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
